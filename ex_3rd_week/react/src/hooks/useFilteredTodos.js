406305accda5aa528a4385bd9880cdffc2dcc288
Simone <morabitosimone99@gmail.com>
2025-07-02 09:23:51 +0200

    completed ex

--- ex_3rd_week/react/src/hooks/useFilteredTodos.js
+++ ex_3rd_week/react/src/hooks/useFilteredTodos.js
@@ -1,20 +1,34 @@
 import { useState, useEffect } from "react";
 
 export const useFilteredTodos = (todos, searchTerm) => {
-  /* const [filteredTodos, setFilteredTodos] = useState([]);
- 
-     useEffect(() => {
-         const filtered = todos.filter(todo =>
-             todo.title.toLowerCase().includes(searchTerm.toLowerCase())
-         );
-         setFilteredTodos(filtered);
-     }, [todos, searchTerm]);
- 
-     return filteredTodos;
- 
-     */
+    const [filteredTodos, setFilteredTodos] = useState([]);
 
-  return todos.filter((todo) =>
-    todo.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+    // crea un 'altra funzione che si occupa di fare il filtro e dentro UseEffect richiami la funzione (dagli come dipendenza solo searchTerm)
+    
+    const filterTodos = () => {
+        return todos.filter (todo => todo.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    }
+
+    useEffect(() => {
+        const filtered = filterTodos();
+        setFilteredTodos(filtered);
+
+    }, [searchTerm]);
+
+    /*  useEffect(() => {
+        const filtered = todos.filter(todo =>
+            todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredTodos(filtered);
+    }, [searchTerm]); */
+
+    return filteredTodos;
+
+
+
+    /* return todos.filter((todo) =>
+      todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  
+    */
 };
